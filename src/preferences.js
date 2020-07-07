c2f02a091c8394e91b5a2d7f356c02545e999aab
Nghia <nphan@fandm.edu>
2020-07-07 18:30:52 -0500

    add comments

--- src/preferences.js
+++ src/preferences.js
@@ -1,7 +1,16 @@
+/**
+ * Retrieve the saved state of a UI element.
+ * @param key the name of the UI element whose state we want to retrieve
+ */
 export const getPreferences = (key) => {
 	return JSON.parse(window.localStorage.getItem(key))
 }
 
+/**
+ * Save the current state of a UI element
+ * @param key the name of the UI element whose state we want to save
+ * @param JSONObject contains the state of the UI element
+ */
 export const savePreferences = (key, JSONObject) => {
 	window.localStorage.setItem(key, JSON.stringify(JSONObject))
 }
